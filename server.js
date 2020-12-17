b577fecb265ceab62202147d218fc9383cf552e5
Sreeganesh <ganuiscool@gmail.com>
2020-12-17 09:39:16 +0530

    Port changedto 80

--- server.js
+++ server.js
@@ -165,6 +165,6 @@ s3.putObject(params, function(err, data) {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
